2275b611763b60be61209f6635f964b59674a53d
YSXCC <wshystudy@163.com>
2021-05-06 12:46:33 +0800

    keyboard

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -10,12 +10,12 @@ int main() {
     put_str("Omnitrix\n");
     init_all();
 
-    thread_start("k_thread_a", 31, k_thread_a, "argA ");
-    thread_start("k_thread_b", 8, k_thread_b, "argB ");
+    // thread_start("k_thread_a", 31, k_thread_a, "argA ");
+    // thread_start("k_thread_b", 8, k_thread_b, "argB ");
 
     intr_enable();
     while (1) {
-        console_put_str("Main ");
+        // console_put_str("Main ");
     }
     return 0;
 }

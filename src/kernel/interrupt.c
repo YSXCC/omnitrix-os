b2208e529aa2a585b7f0857770ad281d897f6332
YSXCC <wshystudy@163.com>
2021-05-08 19:20:33 +0800

    hd driver and printk

--- src/kernel/interrupt.c
+++ src/kernel/interrupt.c
@@ -46,8 +46,8 @@ static void pic_init(void) {
     outb (PIC_S_DATA, 0x01);    //* ICW4: 8086模式, 正常EOI
 
     //* 打开主片上IR0,也就是目前只接受时钟产生的中断
-    outb (PIC_M_DATA, 0xfe);
-    outb (PIC_S_DATA, 0xff);
+    outb (PIC_M_DATA, 0xf8);
+    outb (PIC_S_DATA, 0xbf);
 
     put_str("   pic_init done\n");
 }
